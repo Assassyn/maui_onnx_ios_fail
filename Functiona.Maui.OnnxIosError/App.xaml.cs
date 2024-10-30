c2feacb9264a633f5c90981c6b0f35f541d24721
Szymon Sasin <szymon@be-functional.uk>
2024-10-30 11:40:34 +0000

    Ios build not working example

---
+++ Functiona.Maui.OnnxIosError/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Functiona.Maui.OnnxIosError
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Functiona.Maui.OnnxIosError/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿namespace Functiona.Maui.OnnxIosError
+{
+    using Foundation;
+
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
